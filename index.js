73e180596cae853db80e704f5185cea4c64d04c0
David Fletcher <davidfletcher@Davids-MacBook-Air-2.local>
2021-08-18 23:51:14 -0700

    done! -maybe the stretch is right?!

--- index.js
+++ index.js
@@ -88,14 +88,21 @@ function Car(model, milesPerGallon) {
 //write a method
 Car.prototype.fill = function (gallons){
   this.tank = this.tank + gallons;
-  // you are just reassigning the tank values from before.
+  // you are just reassigning the tank values from before and telling it that you want it to be able to.
 }
 
- Car.prototype.drive = function (distance){
+//STRETCH START::
+
+ Car.prototype.drive = function(distance){
   const milesPossible = this.tank * this.milesPerGallon;
-  if(this.tank === 0){
-    return `the tank has run out`
+  //we need to find 
+  if(distance <= milesPossible){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance /this.milesPerGallon)
+  }else if (this.tank = 0){
+    return `the tank has run out of fuel at ${this.odometer} miles...`;
   }
+  return Car.drive(distance)
 
 } 
 
@@ -107,7 +114,7 @@ Car.prototype.fill = function (gallons){
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
-  //in order to bind this NEW thing(WHAT TO CALL IT??) ******* we need to do the following and identify what has already happened
+  //in order to bind this NEW thing(WHAT TO CALL IT??) ******* we need to do the following and identify what has already happened with .call
   Person.call(this, name, age);
   //something else e define as the following
   this.favoriteToy = favoriteToy;
@@ -120,18 +127,19 @@ Baby.prototype = Object.create(Person.prototype);
 /* NOW We set the function to start at our action word (METHOD - it is an function on the object, 
   therefore method) that we created (in this case 'play') */
 Baby.prototype.play = function(){
-  
+  /* if you wanted to give a conditional here to allow him to play with the favorite toy, what else would you need?
+  Say if he cant play with this toy before he turns "this.age = 8" */
     return `Playing with ${this.favoriteToy}`
-  
 }
 
 /* 
   TASK 4 ** i like these - hope we can get feedback on it before we do the sprint challenge
   In your own words explain the four principles for the "this" keyword below:
-  1. NEW : 
-  2. 
-  3. 
-  4. 
+
+  1. NEW: here, in making new object with constructor functions, "this" refers to the specific instance of the object that is created and returned 
+  2. EXPLICIT : when we use the methods .call() or .apply(), the "this" is EXPLICITLY define bc we are explicity calling them with the new functions aforementioned.
+  3. IMPLICIT: the object to the left of the dot is what this is referring to when the function is invoked. Implicit is really looking at mostly methods wherein OBJECTS are tied to their key's via dot notation. This makes "this" easy to define by saying that it is tied to the object to the left of the dot (console.log)
+  4. WINDOW: if there is no context or object name for the "this" to cling to, the closest thing it can cling to is the window. Meaning that there is no real object to cling to. Therefore this is considered an error.
 */
 
 //https://john-dugan.com/this-in-javascript/
